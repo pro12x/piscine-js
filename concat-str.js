ab5a65bcef3dd481882835fb3b8b7454f939feb7
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-12 13:32:52 +0000

    Piscine-JS-2

---
+++ concat-str.js
@@ -0,0 +1,9 @@
+/*
+    Create a function named concatStr which takes 2 arguments and concatenates them.
+*/
+
+function concatStr(arg1 = "", arg2) {
+    return arg1 + arg2
+}
+
+//console.log(concatStr("Franchis", 12))
