decaaec59cc710d238f9b6946ec1d7171dbf65d2
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-12 11:18:39 +0000

    Piscine-JS-2

---
+++ push.sh
@@ -0,0 +1,10 @@
+git init
+git add .
+git commit -m "Piscine-JS-2"
+#git remote add origin1 https://learn.zone01dakar.sn/git/fmokomba/piscine-js-2.git
+#git remote add origin2 https://github.com/pro12x/piscine-js.git
+#git push -u origin1 master
+#git push -u origin2 master
+git status
+git push origin1 master
+git push origin2 master
