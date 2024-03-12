4fbb4573bfee85411611caffd69a5184c1014435
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-12 11:28:15 +0000

    Piscine-JS-2

---
+++ primitives.js
@@ -0,0 +1,11 @@
+/*
+    Create a constant variable for each of the following primitive types:
+    str for string
+    num for number
+    bool for boolean
+    undef for undefined
+ */
+const str = "Franchis"
+const num = 12
+const bool = true
+const undef = undefined
