ceb945aec10e41dbdf847fe9a083f5b9ea80511b
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-12 13:11:15 +0000

    Piscine-JS-2

---
+++ returns.js
@@ -0,0 +1,15 @@
+/*
+    Create the following functions:
+        id that takes one argument and returns it.
+        getLength that takes an array or a string and returns its length.
+*/
+
+function id(id) {
+    return id
+}
+
+function getLength(arr = []) {
+    return arr.length
+}
+
+//console.log(id(12), getLength([12, 24, 7, 5, 70]))
