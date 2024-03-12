889201805a42756554fb4f0329821f56b6827882
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-12 12:49:17 +0000

    Piscine-JS-2

---
+++ mutability.js
@@ -0,0 +1,21 @@
+/*
+    Create three copies of the person object named clone1, clone2 and samePerson.
+    Increase the age of person by one, and set its country to 'FR'.
+    You must find a way to keep the original values of clone1 and clone2. The values of samePerson should change when person is changed.
+*/
+
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+// Create three copies of the person object named clone1, clone2 and samePerson.
+const clone1 = {...person}
+const clone2 = Object.assign({}, person)
+const samePerson = person
+
+person.age++
+person.country = 'FR'
+
+console.log("Person: ", person, "\nClone 1: ", clone1, "\nClone 2: ", clone2, "\nSame Person: ", samePerson)
