015a938d912a019b4d2843c4f670a27835e0a868
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-03-13 01:00:22 +0000

    Piscine-JS-2

--- physics.js
+++ physics.js
@@ -14,5 +14,14 @@
 */
 
 function getAcceleration(object) {
-
+    if (object.hasOwnProperty("f") && object.hasOwnProperty("m")) {
+        return object.f / object.m
+    } else if (object.hasOwnProperty("Δv") && object.hasOwnProperty("Δt")) {
+        return object.Δv / object.Δt
+    } else if (object.hasOwnProperty("t") && object.hasOwnProperty("d")) {
+        return 2 * object.d / (object.t * object.t)
+    } else {
+        return "impossible"
+    }
 }
+
