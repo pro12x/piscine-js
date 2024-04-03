7d080b13d6c058e2f31492227a9b21479365e334
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2024-04-03 08:48:06 +0000

    Piscine-JS-2

--- personal-shopper.mjs
+++ personal-shopper.mjs
@@ -1,6 +1,6 @@
 'use strict';
 
-import { readdir, readFile, stat, writeFile } from "fs/promises";
+import { readdir, readFile, writeFile } from "fs/promises";
 import { existsSync } from "fs";
 
 const guestDirectory = process.argv[2] ?? './guests';
@@ -22,85 +22,77 @@ let drinkWishers = {
     kebabs: 0,
     potatoes: 0,
 }
-    , guestsCounter = 0
+    , guestsCounter = 0;
 
-function handleTheGuest(guestInfo) {
-    if (guestInfo.answer === 'yes') {
-        guestsCounter++;
-        foodWishers.potatoes++;
-        if (guestInfo.drink) {
-            drinkWishers[guestInfo.drink]++;
-        }
-        switch (guestInfo.food) {
-            case 'veggie':
-            case 'vegan':
-                foodWishers.mushrooms += 3;
-                foodWishers.eggplants++;
-                foodWishers.hummus++;
-                foodWishers.courgettes++;
-                break;
-            case 'carnivore':
-                foodWishers.burgers++;
-                break;
-            case 'fish':
-                foodWishers.sardines++;
-                break;
-            case 'everything':
-                foodWishers.kebabs++;
-                break;
-        }
-    }
-}
-
-
-
-// read a shopping cart from the file or create a new one
-let shoppingList = {};
-if (existsSync(fileShoppingList)) {
-    shoppingList = await readFile(fileShoppingList)
-        .then((content) => content.length > 0 ? JSON.parse(content) : {})
-        .catch((err) => {
+async function main() {
+    let shoppingList = {};
+    if (existsSync(fileShoppingList)) {
+        try {
+            const content = await readFile(fileShoppingList);
+            shoppingList = content.length > 0 ? JSON.parse(content) : {};
+        } catch (err) {
             console.error(new Error(`fail parsing shopping list in ${fileShoppingList}: ${err}\n a new list will be created`));
-        });
-}
-
-// read guests' files and sort out their content
-const filenames = (await readdir(guestDirectory)).filter(filename => filename.endsWith('.json'));
-const promisesHandleGuests = filenames.map(
-    fileName => readFile(`${guestDirectory}/${fileName}`)
-        .then((content) => JSON.parse(content))
-        .then(handleTheGuest)
-);
-await Promise.all(promisesHandleGuests);
-
-if (!guestsCounter) {
-    console.log('No one is coming.');
-    process.exit(0);
-}
-
-if (!shoppingList) {
-    process.exit(0);
-}
-
-// create the shopping list using the data picked from the guests' files
-if (drinkWishers.beer) {
-    shoppingList['6-packs-beers'] = Math.ceil(drinkWishers.beer / 6);
-}
-for (let product of ['water', 'wine', 'soft']) {
-    if (drinkWishers[product]) {
-        shoppingList[`${product}-bottles`] = Math.ceil(drinkWishers[product] / 4);
+        }
     }
+
+    const filenames = (await readdir(guestDirectory)).filter(filename => filename.endsWith('.json'));
+    for (const fileName of filenames) {
+        const guestInfo = JSON.parse(await readFile(`${guestDirectory}/${fileName}`));
+        if (guestInfo.answer === 'yes') {
+            guestsCounter++;
+            foodWishers.potatoes++;
+            if (guestInfo.drink) {
+                drinkWishers[guestInfo.drink]++;
+            }
+            if (guestInfo.food) {
+                switch (guestInfo.food) {
+                    case 'veggie':
+                    case 'vegan':
+                        foodWishers.mushrooms += 3;
+                        foodWishers.eggplants++;
+                        foodWishers.hummus++;
+                        foodWishers.courgettes++;
+                        break;
+                    case 'carnivore':
+                        foodWishers.burgers++;
+                        break;
+                    case 'fish':
+                        foodWishers.sardines++;
+                        break;
+                    case 'everything':
+                        foodWishers.kebabs++;
+                        break;
+                }
+            }
+        }
+    }
+
+    if (!guestsCounter) {
+        console.log('No one is coming.');
+        return;
+    }
+
+    if (drinkWishers.beer) {
+        shoppingList['6-packs-beers'] = Math.ceil(drinkWishers.beer / 6);
+    }
+    for (let product of ['water', 'wine', 'soft']) {
+        if (drinkWishers[product]) {
+            shoppingList[`${product}-bottles`] = Math.ceil(drinkWishers[product] / 4);
+        }
+    }
+
+    for (let product of ['eggplants', 'mushrooms', 'hummus', 'courgettes']) {
+        if (foodWishers[product]) {
+            shoppingList[product] = Math.ceil(foodWishers[product] / 3);
+        }
+    }
+    for (let product of ['burgers', 'sardines', 'kebabs', 'potatoes']) {
+        if (foodWishers[product]) {
+            shoppingList[product] = Math.ceil(foodWishers[product]);
+        }
+    }
+
+    await writeFile(fileShoppingList, JSON.stringify(shoppingList));
 }
 
-for (let product of ['eggplants', 'mushrooms', 'hummus', 'courgettes']) {
-    if (foodWishers[product]) {
-        shoppingList[product] = Math.ceil(foodWishers[product] / 3);
-    }
-}
-for (let product of ['burgers', 'sardines', 'kebabs', 'potatoes']) {
-    if (foodWishers[product]) {
-        shoppingList[product] = Math.ceil(foodWishers[product]);
-    }
-}
-
-await writeFile(fileShoppingList, JSON.stringify(shoppingList));
+main().catch(console.error);
